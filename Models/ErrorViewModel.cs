a938e807a5c4311df4513e011ade6278eae61552
Eric Guo <guochunzhong@thape.com.cn>
2019-03-14 15:55:48 +0800

    Install VS2017 & Run: dotnet new mvc --auth None

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNET_MVC_oidc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
